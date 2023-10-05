9eaf77b031b3b928a43ad5b37374733676aecc93
Sagar3195 <saggyd95@gmail.com>
2023-10-05 13:56:55 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Sagard',
+    author_email= 'saggyd95@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
+
